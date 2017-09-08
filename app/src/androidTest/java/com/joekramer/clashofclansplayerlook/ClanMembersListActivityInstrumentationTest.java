5df26576851007ab04d8527762c3eb91668fde8b
Joe Kramer <joekramer1125@gmail.com>
2017-09-08 15:21:45 -0700

    add espresso tests

--- app/src/androidTest/java/com/joekramer/clashofclansplayerlook/ClanMembersListActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/joekramer/clashofclansplayerlook/ClanMembersListActivityInstrumentationTest.java
@@ -2,9 +2,20 @@ package com.joekramer.clashofclansplayerlook;
 
 
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.core.IsNot.not;
 
 public class ClanMembersListActivityInstrumentationTest {
     @Rule
@@ -12,6 +23,13 @@ public class ClanMembersListActivityInstrumentationTest {
 
     @Test
     public void listItemClickDisplaysToastWithCorrectMember() {
-
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String memberName = "Ryan Moloney";
+        onData(anything())
+                .inAdapterView(withId(R.id.clanMembersListView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(memberName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(memberName)));
     }
 }
