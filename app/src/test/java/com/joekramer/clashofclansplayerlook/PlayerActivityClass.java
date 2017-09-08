f62d188bfbf7a3e6b62391c852e9eea068902470
Joe Kramer <joekramer1125@gmail.com>
2017-09-08 16:58:55 -0700

    refactor tests

--- app/src/test/java/com/joekramer/clashofclansplayerlook/PlayerActivityClass.java
+++ app/src/test/java/com/joekramer/clashofclansplayerlook/PlayerActivityClass.java
@@ -2,26 +2,35 @@ package com.joekramer.clashofclansplayerlook;
 
 
 import android.os.Build;
+import android.widget.ListView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricGradleTestRunner.class)
 public class PlayerActivityClass {
     private PlayerActivity activity;
+    private ListView mClanMembersListView;
+
 
     @Before
     public void setup() {
         activity = Robolectric.setupActivity(PlayerActivity.class);
+        mClanMembersListView = (ListView) activity.findViewById(R.id.clanMembersListView);
+
     }
 
-//    @Test
-//    public void validateTextViewContent() {
-//        TextView titleTextView = (TextView) activity.findViewById(R.id.titleTextView);
-//        assertTrue("Look up Clan's stats".equals(titleTextView.getText().toString()));
-//    }
+    @Test
+    public void clanMembersListViewPopulates() {
+        assertNotNull(mClanMembersListView.getAdapter());
+        assertEquals(mClanMembersListView.getAdapter().getCount(), 7);
+    }
 }
