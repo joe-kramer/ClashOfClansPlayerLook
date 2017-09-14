f46260f8459bdc9ecf7e57d8a40cdb0e8f4368f5
Joe Kramer <joekramer1125@gmail.com>
2017-09-13 20:41:51 -0700

    add models

--- app/src/main/java/com/joekramer/clashofclansplayerlook/models/Clan.java
+++ app/src/main/java/com/joekramer/clashofclansplayerlook/models/Clan.java
@@ -4,8 +4,22 @@ package com.joekramer.clashofclansplayerlook.models;
 public class Clan {
     private String mTag;
     private String mName;
-    private String mClanLevel;
+    private String mDescription;
+    //location object
+    private String mLocation;
+    //badge image comes in small medium and large
     private String mBadgeUrls;
+    private int mClanLevel;
+    private int mClanPoints;
+    private int mClanVersusPoints;
+    private int mRequiredTrophies;
+    private int mWarWinStreak;
+    private int mWarWins;
+    private int mWarTies;
+    private int mWarLosses;
+    private int mMembers;
+    //memberList object
+    private Member[] mMemberList;
 
     public Clan() {
 
---
+++ app/src/main/java/com/joekramer/clashofclansplayerlook/models/Member.java
@@ -0,0 +1,22 @@
+package com.joekramer.clashofclansplayerlook.models;
+
+
+public class Member {
+    private String mTag;
+    private String mName;
+    private String mRole;
+    private int mExpLevel;
+    //from league object
+    private int mLeagueId;
+    private String mLeagueName;
+    private String mLeagueIconUrl;
+
+    private int mTrophies;
+    private int mVersusTrophies;
+    private int mClanRank;
+    private int mDonations;
+    private int mDonationsReceived;
+
+    public Member() {}
+
+}
