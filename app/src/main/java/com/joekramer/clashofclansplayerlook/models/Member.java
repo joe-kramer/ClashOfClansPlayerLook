6b8e70377e6770ed2f63b6e6259796c729be146c
Joe Kramer <joekramer1125@gmail.com>
2017-09-13 20:51:01 -0700

    set getters

--- app/src/main/java/com/joekramer/clashofclansplayerlook/models/Member.java
+++ app/src/main/java/com/joekramer/clashofclansplayerlook/models/Member.java
@@ -17,6 +17,68 @@ public class Member {
     private int mDonations;
     private int mDonationsReceived;
 
-    public Member() {}
+    public Member(String tag, String name, String role, int expLevel, int leagueId,
+                  String leagueName, String leagueIconUrl, int trophies, int versusTrophies,
+                  int clanRank, int donations, int donationsReceived) {
+        this.mTag = tag;
+        this.mName = name;
+        this.mRole = role;
+        this.mExpLevel = expLevel;
+        this.mLeagueId = leagueId;
+        this.mLeagueName = leagueName;
+        this.mLeagueIconUrl = leagueIconUrl;
+        this.mTrophies = trophies;
+        this.mVersusTrophies = versusTrophies;
+        this.mClanRank = clanRank;
+        this.mDonations = donations;
+        this.mDonationsReceived = donationsReceived;
+    }
 
+    public String getTag() {
+        return mTag;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getRole() {
+        return mRole;
+    }
+
+    public int getExpLevel() {
+        return mExpLevel;
+    }
+
+    public int getLeagueId() {
+        return mLeagueId;
+    }
+
+    public String getLeagueName() {
+        return mLeagueName;
+    }
+
+    public String getLeagueIconUrl() {
+        return mLeagueIconUrl;
+    }
+
+    public int getTrophies() {
+        return mTrophies;
+    }
+
+    public int getVersusTrophies() {
+        return mVersusTrophies;
+    }
+
+    public int getClanRank() {
+        return mClanRank;
+    }
+
+    public int getDonations() {
+        return mDonations;
+    }
+
+    public int getDonationsReceived() {
+        return mDonationsReceived;
+    }
 }
