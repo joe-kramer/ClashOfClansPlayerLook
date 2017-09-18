54344077ee076cc023002d01565dd718f7af6aaf
Joe Kramer <joekramer1125@gmail.com>
2017-09-18 14:19:02 -0700

    remove unused variables built from local gradle.properties at runtime

--- app/src/main/java/com/joekramer/clashofclansplayerlook/Constants.java
+++ app/src/main/java/com/joekramer/clashofclansplayerlook/Constants.java
@@ -2,11 +2,7 @@ package com.joekramer.clashofclansplayerlook;
 
 
 public class Constants {
-    public static final String COC_TOKEN_HOT_SPOT = BuildConfig.COC_TOKEN_HOT_SPOT;
-    public static final String COC_BASE_URL = "https://api.clashofclans.com/v1/clans/";
-    public static final String COC_MY_PLAYER_TAG = "%232U2RPQRVL";
-    public static final String COC_TOKEN_TAYLORS_HOUSE = BuildConfig.COC_TOKEN_TAYLORS_HOUSE;
+//    public static final String COC_MY_PLAYER_TAG = "%232U2RPQRVL";
     public static final String COC_MY_CLAN_TAG = "#PO8RVRV9";
-    public static final String COC_TOKEN_ALUMNI_WIFI = BuildConfig.COC_TOKEN_ALUMNI_WIFI;
     public static final String COC_API_GATEWAY_CLANS = "https://rryhypl8c8.execute-api.us-west-2.amazonaws.com/beta/clans";
 }
